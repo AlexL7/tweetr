3e66592367d3d698a997e181df0eeff9a4ca4fbb
Alexander <alexander.lock7@gmail.com>
2016-10-18 21:44:41 +0000

    Hover function working need to refactor it & partial css styling

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,10 +1,9 @@
 $(document).ready(function(){
 
-    console.log("Document Ready");
 
-  var maxlength = 140;
 
   $('textarea').on("keyup", function(){
+    var maxlength = 140;
     var length = $(this).val().length;
     var length = maxlength - length;
     $('span.counter').text(length);
@@ -13,10 +12,22 @@ $(document).ready(function(){
       $('span.counter').addClass("red");
     } else {
       $('span.counter').removeClass("red");
-
     }
-
-
   })
+
+
+  $('article').hover(function(){
+    $('.icons').addClass("appear");
+    }, function(){
+    $('.icons').removeClass("appear");
+  });
+
+  $('article').hover(function(){
+    $('header').addClass("hover");
+    }, function(){
+    $('header').removeClass("hover");
+  });
+
+
 
 });
